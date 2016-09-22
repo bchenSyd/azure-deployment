26dff4cd0e27331c254c8870aebd31ef916e4264
Bo Chen <bochen2014@yahoo.com>
2016-09-23 00:05:22 +1000

    update

--- devServer.js
+++ devServer.js
@@ -17,11 +17,12 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(80, 'localhost', function(err) {
+var port = process.env.port || 1337
+app.listen(port, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost');
+  console.log('Listening at http://localhost:' + port);
 });
